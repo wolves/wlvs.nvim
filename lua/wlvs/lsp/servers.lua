ef7860a3f32096120f326cb03cc0e35f15290089
wolves <cstingl@protonmail.com>
2024-09-28 10:56:26 -0400

    lsp: tsserver -> ts_ls in lspconfig

--- lua/wlvs/lsp/servers.lua
+++ lua/wlvs/lsp/servers.lua
@@ -1,60 +1,60 @@
 local servers = {
-	bashls = {},
-	cssls = {},
-	dockerls = {},
-	gopls = {
-		settings = {
-			gopls = {
-				gofumpt = true,
-			},
-		},
-	},
-	html = {},
-	jsonls = {},
-	lua_ls = {
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-				runtime = {
-					version = "LuaJIT",
-				},
-				telemetry = {
-					enable = false,
-				},
-				workspace = {
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-			},
-		},
-	},
-	nil_ls = {
-		settings = {
-			["nil"] = {
-				formatting = { command = { "alejandra" } },
-			},
-		},
-	},
-	pyright = {
-		settings = {
-			python = {
-				analysis = {
-					autoSearchPaths = true,
-					diagnosticMode = "workspace",
-					useLibraryCodeForTypes = true,
-				},
-			},
-		},
-	},
-	tsserver = {},
-	yamlls = {
-		settings = {
-			yaml = {
-				keyOrdering = false,
-			},
-		},
-	},
+  bashls = {},
+  cssls = {},
+  dockerls = {},
+  gopls = {
+    settings = {
+      gopls = {
+        gofumpt = true,
+      },
+    },
+  },
+  html = {},
+  jsonls = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        runtime = {
+          version = "LuaJIT",
+        },
+        telemetry = {
+          enable = false,
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+      },
+    },
+  },
+  nil_ls = {
+    settings = {
+      ["nil"] = {
+        formatting = { command = { "alejandra" } },
+      },
+    },
+  },
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          useLibraryCodeForTypes = true,
+        },
+      },
+    },
+  },
+  ts_ls = {},
+  yamlls = {
+    settings = {
+      yaml = {
+        keyOrdering = false,
+      },
+    },
+  },
 }
 
 return servers
